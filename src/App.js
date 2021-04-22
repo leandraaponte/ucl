340271238cc28d2d19d6ad13126b2a26916598ea
Leandra Aponte <leandraaponte@hotmail.com>
2021-04-22 13:34:47 -0500

    https://github.com/leandraaponte/ucl.git

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 
 
 function loadData() {
-  return require('./data/mock.json');
+  return require('./data/mock2.0.json');
 }
 function App() {
   const [data, setNewData] = useState(loadData().data);
