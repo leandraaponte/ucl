c737b0bf42b4892f086d32349270cc4e8b08c7aa
Leandra Aponte <leandraaponte@hotmail.com>
2021-04-27 13:02:01 -0500

    https://github.com/leandraaponte/ucl.git

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Legend
+  Legend,
+  BarChart,
+  Bar
 } from "recharts";
 
-
-
+const data01 = [
+  {
+    name: "Week 1",
+    Week: 1.23
+  },
+  {
+    name: "Week 2",
+    Week: 1.52
+  },
+  {
+    name: "Week 3",
+    Week: 0.82
+  },
+  {
+    name: "Week 4",
+    Week: 0.47
+  }
+];
 function loadData() {
   return require('./data/mock2.0.json');
 }
@@ -36,7 +54,7 @@ function App() {
     >
       {/* <XAxis dataKey="name" tick={{fontSize: 20}}/> */}
       <XAxis dataKey="Time" tick={{fontSize: 11,  stroke: 'white'}} label={{ value: "Time (sec)", fontSize: 17, position: "insideBottom", dy: 9, stroke: 'white' }}/>
-      <YAxis tick={{fontSize: 18, stroke: 'white'}}label={{ value: "Velocity (miles/sec)", fontSize: 17, position: "leftup", angle:-90, dx:-12, stroke: 'white' }}/>
+      <YAxis tick={{fontSize: 18, stroke: 'white'}}label={{ value: "Velocity (miles/hr)", fontSize: 17, position: "leftup", angle:-90, dx:-12, stroke: 'white' }}/>
       <Tooltip />
       <Legend />
       <Line type="monotone" dot={false} dataKey="Velocity" stroke="#8884d8" />
@@ -54,13 +72,34 @@ function App() {
     >
       {/* <XAxis dataKey="name" tick={{fontSize: 20}}/> */}
       <XAxis dataKey="Time" tick={{fontSize: 11,  stroke: 'white'}} label={{ value: "Time (sec)", fontSize: 17, position: "insideBottom", dy: 9, stroke: 'white' }}/>
-      <YAxis tick={{fontSize: 18, stroke: 'white'}} label={{ value: "Angle Rotation (deg)", fontSize: 17, position: "leftup", angle:-90, dx:-12, stroke: 'white' }}/>
+      <YAxis tick={{fontSize: 18, stroke: 'white'}} label={{ value: "Angle (deg)", fontSize: 17, position: "leftup", angle:-90, dx:-12, stroke: 'white' }}/>
       <Tooltip />
       <Legend />
       <Line type="monotone" dot={false} dataKey="Shoulder Rotation" stroke="#82ca9d" />
       <Line type="monotone" dot={false} dataKey="Elbow Flexion" stroke="#ff0000" />
       <Line type="monotone" dot={false} dataKey="Arm Slot" stroke="#FFA500" />
     </LineChart>
+    <BarChart
+      width={500}
+      height={400}
+      data={data01}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20
+      }}
+      
+    >
+      <XAxis dataKey="name" scale="band" />
+      <YAxis />
+      <Tooltip />
+      <Bar dataKey="Week" barSize={30} fill="#413ea0" />
+      <Line type="monotone" dot={false} dataKey="Week" stroke="#ff7300" />
+      
+    </BarChart>
+    
+    
     <button onClick={() => setNewData(loadData().data)}>Reload new data</button>
       </header>
     </div>
